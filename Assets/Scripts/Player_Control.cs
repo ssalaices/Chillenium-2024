ef68c0931c8aabf27a25b95888f99e72f285a9b4
GarrettAdams1 <142062639+GarrettAdams1@users.noreply.github.com>
2024-03-02 00:10:06 -0600

    FAILED INTERACTOR

--- Assets/Scripts/Player_Control.cs
+++ Assets/Scripts/Player_Control.cs
@@ -8,7 +8,15 @@ public class Player_Control : MonoBehaviour
     public float movSpeed;
     float speedX, speedY;
     Rigidbody2D rb;
-    
+
+    //for interactor :)
+    private Animator animator;
+    private Vector2 input;
+
+    //might be useful for interactor idk
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -19,11 +27,24 @@ public class Player_Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //also for the interactor i think
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
         speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
         speedY = Input.GetAxisRaw("Vertical") * movSpeed;
         rb.velocity = new Vector2(speedX, speedY);
 
-
+        if (Input.GetKeyDown(KeyCode.F)){
+            Interact();
+        }
     }
+
+    void Interact() {
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var interactPos = transform.position + facingDir;
+
+        Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
+    }
+
 }
