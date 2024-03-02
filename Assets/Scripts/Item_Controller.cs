15c9f0cbc391cf52dce927dd5d695659c8583928
GarrettAdams1 <142062639+GarrettAdams1@users.noreply.github.com>
2024-03-01 23:13:10 -0600

    Interactor 1

---
+++ Assets/Scripts/Item_Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Controller : MonoBehaviour, Interactable
+{
+   public void Interact(){
+        Debug.Log("NICE!");
+   }
+
+} 
---
+++ Assets/Scripts/Object_Controller.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Controller : MonoBehaviour, Interactable
+{
+    public void Interact(){
+        Debug.Log("Your so cool");
+    }
+}
