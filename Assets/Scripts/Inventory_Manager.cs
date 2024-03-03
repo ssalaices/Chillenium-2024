9a17295f14acd9971a831575e75fbd08365b466e
Thomas Masha <thomas8483828180@gmail.com>
2024-03-02 21:12:23 -0600

    Inventory

--- Assets/Scripts/Inventory_Manager.cs
+++ Assets/Scripts/Inventory_Manager.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public int[] inventory = new int[12];
+    public int balance = 0;
     public void AddItem(int type)
     {
         ++inventory[type];
+    }
+    public void SellItem(int type, int count) {
+        if (count <= inventory[type]) {
+            for (int i = 0; i < count; ++i) {
+                balance += Random.Range(50, 101);
+            }
+        } else Debug.Log("Tried to sold more than he had");
     }
 }
