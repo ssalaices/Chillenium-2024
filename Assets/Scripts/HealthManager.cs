59128a38e1afb7ec405074ea31febc8047a66062
GarrettAdams1 <142062639+GarrettAdams1@users.noreply.github.com>
2024-03-02 18:35:07 -0600

    Combat v7

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
 public class HealthManager : MonoBehaviour
 {
+    public Image healthBar;
+    public float healthAmount = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,37 @@ public class HealthManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (healthAmount <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TakeDamage(20);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Heal(5);
+        }
+
+
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        healthAmount -= damage;
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount += healingAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+        healthBar.fillAmount = healthAmount / 100f;
     }
 }
