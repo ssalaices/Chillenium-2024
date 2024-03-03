579e31325d2e1fb4614daa2793709f6faf3f570b
Thomas Masha <thomas8483828180@gmail.com>
2024-03-03 11:26:32 -0600

    shop gui

--- Assets/Scripts/Inventory_Manager.cs
+++ Assets/Scripts/Inventory_Manager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public int[] inventory = new int[12];
-    public int debt = 0;
+    public int debt;
+    void Start()
+    {
+        debt = 1500;
+    }
     public void AddItem(int type) {
         ++inventory[type];
     }
     public void RemoveItem(int type) {
-        if (type >= 1) --inventory[type];
+        --inventory[type];
     }
 }
